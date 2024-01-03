87547a7dbd7bf21c1400fb0a3a271506956e1ce2
Chau Ngo <chau_ngo@claris.com>
2024-01-03 15:31:27 -0800

    add QA model

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from model import summary_pipline
+from model import summary_pipline, question_anwser
 from pydantic import BaseModel
 from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
-class TextIn(BaseModel):
+class ArticleIn(BaseModel):
     ARTICLE: str
+
+class QAIn(BaseModel):
+    question: str
+    context: str
 
 @app.get("/")
 def read_root():
     return {"Hello": "Claris"}
  
 @app.post("/summary")
-def summary(payload: TextIn):
+def summary(payload: ArticleIn):
     return summary_pipline(payload.ARTICLE)
+
+@app.post("/questionanswer")
+def answer(payload: QAIn):
+    return question_anwser(payload.question, payload.context)
